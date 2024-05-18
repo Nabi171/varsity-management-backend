4efc5b90ec56a17d7c49cb7dfc54430f0ecb201e
Md Nabiul Bashar <nabiultasbed@gmail.com>
2024-05-18 18:50:59 +0600

    changes in root file where it make change in modules

---
+++ src/modules/student/student.model.ts
@@ -0,0 +1,143 @@
+import { Schema, model } from 'mongoose';
+import validator from 'validator';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from './student.interface';
+
+const userNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: [true, 'firstName is needed!'],
+    trim: true, //first name space dia likele eigula asbe na
+    maxLength: [20, 'Name can not be more than 20 characters'],
+    // validate: {
+    //   //custom validation
+    //   validator: function (value: string) {
+    //     const firstNameStr = value.charAt(0).toUpperCase() + str.slice(1);
+    //     // console.log(value);
+    //     if (value !== firstNameStr) {
+    //       return false;
+    //     }
+    //     return true;
+    //   },
+    //   message: '{VALUE} is not in capitalize format',
+    // },
+
+    validate: {
+      validator: (value: string) => validator.isAlpha(value),
+      message: '{VALUE} is not valid',
+    },
+  },
+  middleName: {
+    type: String,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'LastName is needed!'],
+    validate: {
+      validator: (value: string) => validator.isAlpha(value),
+      message: '{VALUE} is not valid',
+    },
+  },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: {
+    type: String,
+    required: true,
+  },
+  fatherOccupation: {
+    type: String,
+    required: true,
+  },
+  fatherContactNo: {
+    type: String,
+    required: true,
+  },
+  motherName: {
+    type: String,
+    required: true,
+  },
+  motherOccupation: {
+    type: String,
+    required: true,
+  },
+  motherContactNo: {
+    type: String,
+    required: true,
+  },
+});
+
+const localGuradianSchema = new Schema<LocalGuardian>({
+  name: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String, required: true, unique: true },
+  name: {
+    type: userNameSchema,
+    required: true,
+  },
+  gender: {
+    type: String,
+    // enum: ['male', 'female', 'other'],
+    enum: {
+      values: ['male', 'female', 'other'],
+      message: '{VALUE} is not valid',
+    },
+    required: true,
+  },
+  dateOfBirth: { type: String },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (value: string) => validator.isEmail(value),
+      message: '{VALUE} is not a valid email type',
+    },
+  },
+  contactNo: { type: String, required: true },
+  emergencyContactNo: { type: String, required: true },
+  bloogGroup: {
+    type: String,
+    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  },
+  presentAddress: { type: String, required: true },
+  permanentAddres: { type: String, required: true },
+  guardian: {
+    type: guardianSchema,
+    required: true,
+  },
+  localGuardian: {
+    type: localGuradianSchema,
+    required: true,
+  },
+  profileImg: { type: String },
+  isActive: {
+    type: String,
+    enum: ['active', 'blocked'],
+    default: 'active',
+  },
+});
+
+export const StudentModel = model<Student>('Student', studentSchema);
---
+++ text.md
@@ -0,0 +1,8 @@
+=>npm init-y,npm i express,npm i mongoose,npm i typescript --save-dev,npm i cors,npm i dotenv,tsc -init(change root and outdir),
+=>optional-pacakge.json e tsc:build leke "npm run build "dile dist file create hbe ,then caile dist er modde js file take run korte paro
+=>connect mongoose in server.ts from starter pack in mongose,then mongodb online database,npm i mongodb>admin atlas role hbe>
+4=>npm i --save-dev @types/express
+5=>npm i --save-dev @types/cors
+route=>controller>service>req,res
+
+=>install validator for the err validation
