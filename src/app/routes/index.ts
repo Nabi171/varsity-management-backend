a029c83432ce21118bbd3b9c568812eda3a6d9ee
Md Nabiul Bashar <nabiultasbed@gmail.com>
2024-05-29 22:40:45 +0600

    change in add interface

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,3 +1,4 @@
+import { AcademicFacultyRoutes } from './../modules/academicFaculty/academicFaculty.route';
 import { Router } from 'express';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
 import { StudentRoutes } from '../modules/student/student.route';
@@ -17,6 +18,10 @@ const moduleRoutes = [
   {
     path: '/academic-semesters',
     route: AcademicSemesterRoutes,
+  },
+  {
+    path: '/academic-faculties',
+    route: AcademicFacultyRoutes,
   },
 ];
 
