ab14364488a80a9ecd3e3347f1c30be85036547e
Md Nabiul Bashar <nabiultasbed@gmail.com>
2024-05-29 12:00:51 +0600

    changes at validation.ts

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,9 +1,21 @@
-import { invalid } from "joi";
+import { z } from 'zod';
 
-const createAcademicFacultyValidationSchema=z.object({
-    body.z.object({
-        name:z.string({
-            invalid_type_error:"Academic Faculty must be string";
-        })
-    })
-})
+const createAcademicFacultyValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic faculty must be string',
+    }),
+  }),
+});
+
+const updateAcademicFacultyValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic faculty must be string',
+    }),
+  }),
+});
+
+export const AcademicFacultyValidation = {
+  createAcademicFacultyValidationSchema,
+};
