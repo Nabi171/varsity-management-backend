b219e178c9198dfd0b0b813e192243a9b0624ce7
Md Nabiul Bashar <nabiultasbed@gmail.com>
2024-05-30 22:39:53 +0600

    student service updated

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,4 +1,4 @@
-import { httpStatus } from 'http-status';
+import httpStatus from 'http-status';
 import mongoose from 'mongoose';
 import { Student } from './student.model';
 import AppError from '../../errors/AppError';
@@ -17,7 +17,7 @@ const getAllStudentsFromDB = async () => {
 };
 
 const getSingleStudentFromDB = async (id: string) => {
-  const result = await Student.findById(id)
+  const result = await Student.findOne({ id })
     .populate('admissionSemester')
     .populate({
       path: 'academicDepartment',
