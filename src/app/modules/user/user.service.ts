b7d510cfcb5574c1932570a23b134513c19a1d57
Md Nabiul Bashar <nabiultasbed@gmail.com>
2024-05-31 22:00:40 +0600

    change in user service 1

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -83,7 +83,7 @@ const createStudentIntoDB = async (password: string, payload: TStudent) => {
 //     studentData.id = newUser.id;
 //     studentData.user = newUser._id; //reference _id
 
-//     const newStudent = await Student.create
+//     const newStudent = await Student.crea
 
 export const UserServices = {
   createStudentIntoDB,
